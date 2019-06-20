fe12ba19c385af7c546cfab9f8efe7740ab45f17
Farid Joubbi <farid@joubbi.se>
2019-06-20 14:35:37 +0200

    Made if posix

--- snmp_cisco_info.sh
+++ snmp_cisco_info.sh
@@ -48,27 +48,27 @@ print_and_exit (){
 
   # OID does not exist
   echo "$hostname" | /bin/grep -E 'exists|available' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     hostname="N/A"
   fi
   echo "$model" | /bin/grep -E 'exists|available' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     model="N/A"
   fi
   echo "$version" | /bin/grep -E 'exists|available' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     version="N/A"
   fi
   echo "$serial" | /bin/grep -E 'exists|available' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     serial="N/A"
   fi
   echo "$location" | /bin/grep -E 'exists|available' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     location="N/A"
   fi
   echo "$contact" | /bin/grep -E 'exists|available' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     contact="N/A"
   fi
 
@@ -101,11 +101,11 @@ print_and_exit (){
 # Make sure that this points to snmpget
 SNMPGET="/usr/bin/snmpget"
 
-if [ $# == 6 ]; then
+if [ $# = 6 ]; then
   SNMPOPT="-v 3 -l authPriv -u $2 -a $3 -A $4 -x $5 -X $6 $1 -Ov -t 0.5 -Lo"
 fi 
 
-if [ $# == 2 ]; then
+if [ $# = 2 ]; then
   SNMPOPT="-v 2c -c $2 $1 -Ov -t 0.5 -Lo"
 fi
 
@@ -134,7 +134,7 @@ fi
 hostname=`echo "$hostname" | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
 # Handle SNMP error (Usually due to no SNMPv3 support)
 echo "$hostname" | /bin/grep 'snmpget' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   hostname="N/A"
   model="N/A"
   version="N/A"
@@ -153,7 +153,7 @@ model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.2.1001 | /bin/sed -e 's/\STRING
 
 # 9500. Maybe there is a better way to do this in the future looking at the version instead
 echo "$model" | /bin/grep 'PSOC' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.13.1000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   version=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.1000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[,]' '_'`
   serial=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.1000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
@@ -162,13 +162,13 @@ fi
 
 # 6807
 echo "$model" | /bin/grep '6807' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.13.1000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   version=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.3000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[,]' '_'`
   serial=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.1000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.2000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   echo "$serial2" | /bin/grep -v 'exists' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     model2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.13.2000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_i'`
     serial=`printf "%s, %s" "$serial" "$serial2"`
     model=`printf "%s, %s" "$model" "$model2"`
@@ -183,13 +183,13 @@ serial=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.1001 | /bin/sed -e 's/\STRI
 
 # Cisco routers
 echo "$version" | /bin/grep 'exists' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   version=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.1 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[,]' '_'`
 fi
 
 # Cisco Nexus
 echo "$version" | /bin/grep 'exists' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   version=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.22 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[,]' '_'`
 fi
 
@@ -205,7 +205,7 @@ fi
 
 # Cisco 4500 new IOS and C6807-XL
 echo "$version" | /bin/grep 'exists' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   version=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.3000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[,]' '_'`
 fi
 
@@ -214,19 +214,19 @@ fi
 
 # Cisco routers
 echo "$model" | /bin/grep 'exists' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.2.1 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
 fi
 
 # Cisco Nexus
 echo "$model" | /bin/grep 'exists' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.13.10 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
 fi
 
 # Some old Cisco ASR IOS
 echo "$model" | /bin/grep 'V1' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.2.1 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
 fi
 
@@ -235,13 +235,13 @@ fi
 
 # Cisco routers
 echo "$serial" | /bin/grep 'exists' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   serial=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.1 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
 fi
 
 # Cisco Nexus
 echo "$serial" | /bin/grep 'exists' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   serial=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.10 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
 fi
 
@@ -250,12 +250,12 @@ fi
 
 # Cisco 4500 VSS
 $SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.2.2 | /bin/grep 'WS-C45' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.2.2 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | /bin/sed -e 's/\Cisco Systems, Inc. //g' | cut -d' ' -f1`
   serial=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.2 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.500 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   echo "$serial2" | /bin/grep -v 'exists' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     model2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.2.500 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | /bin/sed -e 's/\Cisco Systems, Inc. //g' | cut -d' ' -f1`
     serial=`printf "%s, %s" "$serial" "$serial2"`
     model=`printf "%s, %s" "$model" "$model2"`
@@ -277,34 +277,34 @@ fi
 
 # Cisco 6500
 echo "$model" | /bin/grep 'Chassis' | /bin/grep -Ev 'C3850|ASR' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.7.1000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   serial=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.1000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   echo "$model" | /bin/grep -v 'WS' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.7.2 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     serial=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.2 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   fi
   model2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.7.2000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.2000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   echo "$model2" | /bin/grep -v 'WS' > /dev/null
-  if [ $? == 0 ]; then
+  if [ $? = 0 ]; then
     model2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.7.3000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.3000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     echo "$model2" | /bin/grep -v 'WS' > /dev/null
-    if [ $? == 0 ]; then
+    if [ $? = 0 ]; then
       model2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.7.4000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
       serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.4000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
       echo "$model2" | /bin/grep -v 'WS' > /dev/null
-      if [ $? == 0 ]; then
+      if [ $? = 0 ]; then
         model2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.7.5000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
         serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.5000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
         echo "$model2" | /bin/grep -v 'WS' > /dev/null
-        if [ $? == 0 ]; then
+        if [ $? = 0 ]; then
           model2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.7.6000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
           serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.6000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
           echo "$model2" | /bin/grep -v 'WS' > /dev/null
-          if [ $? == 0 ]; then
+          if [ $? = 0 ]; then
             model2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.7.7000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
             serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.7000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
           fi
@@ -320,12 +320,12 @@ fi
 
 # Cisco C3850
 echo "$model" | /bin/grep -E 'C3850|StackPort' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   model=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.2.1000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   serial=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.1000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.2000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   echo "$serial2" | /bin/grep 'exists' > /dev/null
-  if [ $? == 1 ]; then
+  if [ $? = 1 ]; then
     serial=`printf "%s, %s" "$serial" "$serial2"`
     version2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.2000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     version=`printf "%s, %s" "$version" "$version2"`
@@ -333,7 +333,7 @@ if [ $? == 0 ]; then
     model=`printf "%s, %s" "$model" "$model2"`
     serial3=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.3000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     echo "$serial3" | /bin/grep 'exists' > /dev/null
-    if [ $? == 1 ]; then
+    if [ $? = 1 ]; then
       serial=`printf "%s, %s" "$serial" "$serial3"`
       version3=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.3000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
       version=`printf "%s, %s" "$version" "$version3"`
@@ -341,7 +341,7 @@ if [ $? == 0 ]; then
       model=`printf "%s, %s" "$model" "$model3"`
       serial4=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.4000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
       echo "$serial4" | /bin/grep 'exists' > /dev/null
-      if [ $? == 1 ]; then
+      if [ $? = 1 ]; then
         serial=`printf "%s, %s" "$serial" "$serial4"`
         version4=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.4000 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
         version=`printf "%s, %s" "$version" "$version4"`
@@ -355,10 +355,10 @@ fi
 
 # Cisco C2960S, C2960X and C3750 stack members
 echo "$model" | /bin/grep -E 'C2960S|C2960X|C3750' > /dev/null
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
   serial2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.2001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
   echo "$serial2" | /bin/grep 'exists' > /dev/null
-  if [ $? == 1 ]; then
+  if [ $? = 1 ]; then
     serial=`printf "%s, %s" "$serial" "$serial2"`
     version2=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.2001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     version=`printf "%s, %s" "$version" "$version2"`
@@ -366,7 +366,7 @@ if [ $? == 0 ]; then
     model=`printf "%s, %s" "$model" "$model2"`
     serial3=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.3001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
     echo "$serial3" | /bin/grep 'exists' > /dev/null
-    if [ $? == 1 ]; then
+    if [ $? = 1 ]; then
       serial=`printf "%s, %s" "$serial" "$serial3"`
       version3=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.2001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
       version=`printf "%s, %s" "$version" "$version3"`
@@ -374,7 +374,7 @@ if [ $? == 0 ]; then
       model=`printf "%s, %s" "$model" "$model3"`
       serial4=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.4001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
       echo "$serial4" | /bin/grep 'exists' > /dev/null
-      if [ $? == 1 ]; then
+      if [ $? = 1 ]; then
         serial=`printf "%s, %s" "$serial" "$serial4"`
         version4=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.2001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
         version=`printf "%s, %s" "$version" "$version4"`
@@ -382,7 +382,7 @@ if [ $? == 0 ]; then
         model=`printf "%s, %s" "$model" "$model4"`
 	serial5=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.5001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
         echo "$serial5" | /bin/grep 'exists' > /dev/null
-        if [ $? == 1 ]; then
+        if [ $? = 1 ]; then
           serial=`printf "%s, %s" "$serial" "$serial5"`
           version5=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.2001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
           version=`printf "%s, %s" "$version" "$version5"`
@@ -390,7 +390,7 @@ if [ $? == 0 ]; then
           model=`printf "%s, %s" "$model" "$model5"`
           serial6=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.6001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
           echo "$serial6" | /bin/grep 'exists' > /dev/null
-          if [ $? == 1 ]; then
+          if [ $? = 1 ]; then
             serial=`printf "%s, %s" "$serial" "$serial6"`
             version6=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.2001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
             version=`printf "%s, %s" "$version" "$version6"`
@@ -398,7 +398,7 @@ if [ $? == 0 ]; then
             model=`printf "%s, %s" "$model" "$model6"`
             serial7=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.7001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
             echo "$serial7" | /bin/grep 'exists' > /dev/null
-            if [ $? == 1 ]; then
+            if [ $? = 1 ]; then
               serial=`printf "%s, %s" "$serial" "$serial7"`
               version7=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.2001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
               version=`printf "%s, %s" "$version" "$version7"`
@@ -406,7 +406,7 @@ if [ $? == 0 ]; then
               model=`printf "%s, %s" "$model" "$model7"`
               serial8=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.11.8001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
               echo "$serial8" | /bin/grep 'exists' > /dev/null
-              if [ $? == 1 ]; then
+              if [ $? = 1 ]; then
                 serial=`printf "%s, %s" "$serial" "$serial8"`
                 version8=`$SNMPGET $SNMPOPT .1.3.6.1.2.1.47.1.1.1.1.10.2001 | /bin/sed -e 's/\STRING: //g' | /bin/sed -e 's/\"//g' | tr '[<>]' '_'`
                 version=`printf "%s, %s" "$version" "$version8"`
